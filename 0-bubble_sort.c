b24724d21d99739bea6054335ade053f99f522af
henok1992 <henokgithub@gmail.com>
2022-10-11 11:01:15 -0700

    whebflj

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,31 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers with Bubble Sort
- * @array: param
- * @size: param
- *
- * Return: void
- */
+*bubble_sort - bubble sort
+*@array: array to be sorted
+*@size: size of array
+*Return: Nothing
+*/
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, n, n1;
-    int swap;
-
-    if (array == NULL || size < 2)
-        return;
-
-    n = size;
-    while (n > 0)
-    {
-        n1 = 0;
-        for (i = 0; i < n - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                swap = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = swap;
-                n1 = i + 1;
-                print_array(array, size);
-            }
-        }
-        n = n1;
-    }
+size_t i, j;
+int temp, swap = 0;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size; i++)
+{
+swap = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swap = 1;
+print_array(array, size);
+}
+}
+if (swap == 0)
+break;
+}
 }
